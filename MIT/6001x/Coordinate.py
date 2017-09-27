c0873e19022486dda03ec7cc3d0fa662e4a215da
saurabhm3hra <saurabhm3hra@gmail.com>
2017-09-27 17:57:19 +0800

    Classes Inheritance

---
+++ MIT/6001x/Coordinate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 16:04:52 2017
+
+@author: saurabhmehra
+"""
+
+class Coordinate(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+    
+    def __eq__(self, other):
+        if self.getX() == other.getX() and self.getY() == other.getY():
+            return True
+        else:
+            return False
+    
+    def __repr__(self):
+        return "Coordinate(%d,%d)" %(self.getX(), self.getY())
